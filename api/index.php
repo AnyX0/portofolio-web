62d4077c0bbd5908bb0a879f1db322cae7312d4d
Andi <andi@portofolio-anyx.com>
2026-01-18 13:22:44 +0700

    fix: remove duplicate LARAVEL_START constant definition

--- api/index.php
+++ api/index.php
@@ -1,8 +1,4 @@
 <?php
 
-// Vercel serverless handler untuk Laravel
-// Definisikan LARAVEL_START untuk Laravel bootstrapping
-define('LARAVEL_START', microtime(true));
-
 // Bootstrap Laravel application
 require __DIR__ . '/../public/index.php';
